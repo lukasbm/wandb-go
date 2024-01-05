5bcf5d0f7f7ac7983a97f0d3d972c32150f05824
Lukas Boehm <lukas.l.boehm@fau.de>
2024-01-05 17:35:18 +0100

    :truck: move queries

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/Khan/genqlient/graphql"
+	"net/http"
+)
+
+//go:generate go run github.com/Khan/genqlient
+
 func main() {
+	ctx := context.Background()
+	client := graphql.NewClient("https://api.wandb.ai/graphql", http.DefaultClient)
+
+	resp, err := FetchAProject(ctx, client)
+	fmt.Println(resp.GetProject(), err)
 }
